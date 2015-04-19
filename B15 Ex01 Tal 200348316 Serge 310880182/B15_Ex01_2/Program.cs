7f0b328dcae9bc404ebea21eb352c14bda48908e
TalKashi <shakikashi@gmail.com>
2015-04-19 23:00:19 +0300

    Solution ex01_2

---
+++ B15 Ex01 Tal 200348316 Serge 310880182/B15_Ex01_2/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace B15_Ex01_2
+{
+    class Program
+    {
+        static void Main()
+        {
+            PrintStandartSandClock();
+            Console.ReadLine();
+        }
+
+        public static void PrintStandartSandClock()
+        {
+            string output = 
+@"*****
+ ***
+  *
+ ***
+*****";
+
+            Console.WriteLine(output);
+        }
+    }
+}
