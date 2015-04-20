e23575829a2c606b65f9cfb801067a99519e8039
TalKashi <shakikashi@gmail.com>
2015-04-20 19:55:34 +0300

    Ex01_02 and Ex01_03 updated

--- B15 Ex01 Tal 200348316 Serge 310880182/B15_Ex01_2/Program.cs
+++ B15 Ex01 Tal 200348316 Serge 310880182/B15_Ex01_2/Program.cs
@@ -1,27 +1,50 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Text;
 
 namespace B15_Ex01_2
 {
-    class Program
+    public class Program
     {
-        static void Main()
+        public static void Main()
         {
-            PrintStandartSandClock();
-            Console.ReadLine();
+            PrintSandClock(5);
         }
 
-        public static void PrintStandartSandClock()
+        public static void PrintSandClock(int i_NumOfLinesInClock)
         {
-            string output = 
-@"*****
- ***
-  *
- ***
-*****";
+            StringBuilder stringBuilder = new StringBuilder();
+            // Handle even input
+            if (i_NumOfLinesInClock % 2 == 0)
+            {
+                i_NumOfLinesInClock++;
+            }
 
-            Console.WriteLine(output);
+            int spaceFromStart = 0;
+            for (int i = i_NumOfLinesInClock; i > 2; i -= 2)
+            {
+                generateStars(stringBuilder, i, spaceFromStart);
+                spaceFromStart++;
+            }
+            generateStars(stringBuilder, 1, spaceFromStart);
+            for (int i = 3; i <= i_NumOfLinesInClock; i += 2)
+            {
+                spaceFromStart--;
+                generateStars(stringBuilder, i, spaceFromStart);
+            }
+            Console.WriteLine(stringBuilder.ToString());
+        }
+
+        private static void generateStars(StringBuilder i_StringBuilder, int i_NumOfStars, int i_SpaceFromStart)
+        {
+            for (int i = 0; i < i_SpaceFromStart; i++)
+            {
+                i_StringBuilder.Append(" ");
+            }
+            for (int i = 0; i < i_NumOfStars; i++)
+            {
+                i_StringBuilder.Append("*");
+            }
+            i_StringBuilder.AppendLine();
         }
     }
 }
---
+++ B15 Ex01 Tal 200348316 Serge 310880182/B15_Ex01_3/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace B15_Ex01_3
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            int numOfLinesInClock = readInputFromUser();
+            B15_Ex01_2.Program.PrintSandClock(numOfLinesInClock);
+            Console.ReadKey();
+        }
+
+        private static int readInputFromUser()
+        {
+            int numOfLinesInClock = 0;
+            Console.WriteLine("Please enter the number of lines for the sand clock:");
+            string inputStr = Console.ReadLine();
+            while (!int.TryParse(inputStr, out numOfLinesInClock) || numOfLinesInClock < 0)
+            {
+                Console.WriteLine("The input you entered is invalid. Please try again.");
+                inputStr = Console.ReadLine();
+            }
+
+            return numOfLinesInClock;
+        }
+    }
+}
