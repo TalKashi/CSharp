8897c4c491659f774b89f952f9b5e5daa8842e74
SergeOxe <sergeoxe@gmail.com>
2015-05-22 12:48:06 +0300

    working on menu

--- B15 Ex03 TalKashi 200348316 SergeOksenoid 310880182/Ex03.GarageManagementSystem.ConsoleUI/GarageMenu.cs
+++ B15 Ex03 TalKashi 200348316 SergeOksenoid 310880182/Ex03.GarageManagementSystem.ConsoleUI/GarageMenu.cs
@@ -1,13 +1,27 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using Ex03.GarageLogic;
 
 namespace Ex03.GarageManagementSystem.ConsoleUI
 {
     class GarageMenu
     {
+        private Garage m_grage;
+
+        public GarageMenu()
+        {
+            m_grage = new Garage();
+        }
+
         public void AddVehicle() 
         {
+            Console.WriteLine("Please enter your name ");
+            string ownerName = Console.ReadLine();
+
+            Console.WriteLine("Please enter your phone");
+            string ownerPhone = Console.ReadLine();
+
             Console.WriteLine("Please enter license plate");
             string licesePlate = Console.ReadLine();
 
@@ -21,35 +35,35 @@ namespace Ex03.GarageManagementSystem.ConsoleUI
         }
 
 
-        public void DisplayVehicleLicensePlateByStatus(int i_status) 
+        public void DisplayVehicleLicensePlateByStatus(eStatus i_status) 
         {
-        
+            m_grage.GetLicenseNumbersByStatus(i_status);
         }
 
-        public void ChangeVehicleStatus(string i_licensePlate, string i_status)
+        public void ChangeVehicleStatus(string i_licensePlate, eStatus i_status)
         {
-
+            m_grage.ChangeVehicleStatus(i_licensePlate, i_status);
         }
 
         public void PumpAirToMax(string i_licensePlate)
         {
-
+            m_grage.PumpAirInWheels(i_licensePlate);
         }
 
 
-        public void AddFuel(string i_licensePlate, string i_fuelType, float i_amount) 
+        public void AddFuel(string i_licensePlate, eFuelType i_fuelType, float i_amount) 
         {
-        
+            m_grage.PumpFuel(i_licensePlate, i_fuelType, i_amount);
         }
 
         public void ChargeBettary(string i_licensePlate, float i_amount) 
         {
-        
+            m_grage.ChargeBettery(i_licensePlate, i_amount);
         }
 
         public void DisplayInfo(string i_licensePlate) 
         {
-        
+            m_grage.GetVehicleDetails(i_licensePlate);
         }
 
 
@@ -58,11 +72,12 @@ namespace Ex03.GarageManagementSystem.ConsoleUI
             int userChoose;
             StringBuilder vehicleMenu;
             //get vehicle list
-            vehicleList =  Garage.getVehicleList;
-            int i = 0;
+            vehicleList = m_grage.GetVehicleList();
+
             for (int i = 0 ; i < getVehicleList.lenght ; i++)
             {
                vehicleMenu.Append(string.Format("For {0} press {1}",getVehicleList[i],i));
+               vehicleMenu.Append(Environment.NewLine);
             }
             Console.WriteLine(vehicleMenu.ToString());
             string vehicleType = Console.ReadLine();
@@ -96,15 +111,9 @@ Press 1 for Electric Engine");
         {
             bool isValid  = false;
             o_userChoise = -1;
-            try 
-            {
-               o_userChoise = Convert.ToInt32(i_userInput);
-               isValid = (0 <= o_userChoise &&  o_userChoise < i_upperRangeBound);
+            if (Int32.TryParse(i_userInput,out o_userChoise)){
+                isValid = (0 <= o_userChoise &&  o_userChoise < i_upperRangeBound);
             }
-            catch (ArgumentException ae)
-            {
-            }
-
             return isValid;
         }
 
--- B15 Ex03 TalKashi 200348316 SergeOksenoid 310880182/Ex03.GarageManagementSystem.ConsoleUI/Program.cs
+++ B15 Ex03 TalKashi 200348316 SergeOksenoid 310880182/Ex03.GarageManagementSystem.ConsoleUI/Program.cs
@@ -8,6 +8,33 @@ namespace Ex03.GarageManagementSystem.ConsoleUI
     {
         public static void Main()
         {
+
+            while (true)
+            {
+                displayMenu();
+                getInputFromUser();
+            }
+
         }
+
+        private static void displayMenu()
+        {
+            string[] options = {"adding new vehicle to the grage",
+                                  "display vehicles by thier status",
+                                  "changing vehicle status",
+                                  "pump air","pump fuel",
+                                  "charge bettary",
+                                   "display info"};
+            for (int i = 0; i < options.Length; i++)
+            {
+                Console.WriteLine(string.Format("Press {0} for {1}",i,options[i]));
+            }
+        }
+
+        private static void getInputFromUser()
+        {
+
+        }
+
     }
 }
