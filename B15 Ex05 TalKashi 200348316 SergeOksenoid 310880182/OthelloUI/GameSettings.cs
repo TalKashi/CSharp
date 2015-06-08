31f6587fed72e6fd07f20292379a73bac15ad642
TalKashi <shakikashi@gmail.com>
2015-06-08 14:39:20 +0300

    Start experimenting

---
+++ B15 Ex05 TalKashi 200348316 SergeOksenoid 310880182/OthelloUI/GameSettings.cs
@@ -0,0 +1,42 @@
+﻿using System.Windows.Forms;
+using System.Drawing;
+
+namespace EX5.OthelloGame.OthelloUI
+{
+    internal class GameSettings : Form
+    {
+        private Button m_BoardSizeButton;
+        private Button m_PlayVsComputerButton;
+        private Button m_PlayVsHumanButton;
+
+        public GameSettings()
+        {
+            this.Size = new Size(500, 200);
+            this.StartPosition = FormStartPosition.CenterScreen;
+            this.FormBorderStyle = FormBorderStyle.Fixed3D;
+            this.MaximizeBox = false;
+            initializeComponents();
+        }
+
+        private void initializeComponents() 
+        {
+            this.Text = "Othello - Game Settings";
+
+            m_BoardSizeButton = new Button();
+            m_BoardSizeButton.Text = "Board Size: 6x6 (click to increase)";
+            m_BoardSizeButton.Height = this.ClientSize.Height / 2 - 10;
+            m_BoardSizeButton.Width = this.ClientSize.Width - 20;
+            m_BoardSizeButton.Top = 10;
+            m_BoardSizeButton.Left = 10;
+
+            m_PlayVsComputerButton = new Button();
+            m_PlayVsComputerButton.Text = "Play against the computer";
+            
+            m_PlayVsHumanButton = new Button();
+            m_PlayVsHumanButton.Text = "Play against your friend";
+
+            this.Controls.AddRange(new Control[] {m_BoardSizeButton, m_PlayVsComputerButton, m_PlayVsHumanButton});
+        }
+
+    }
+}
---
+++ B15 Ex05 TalKashi 200348316 SergeOksenoid 310880182/OthelloUI/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EX5.OthelloGame.OthelloUI
+{
+    class Program
+    {
+        static void Main()
+        {
+            new GameSettings().ShowDialog();
+        }
+    }
+}
